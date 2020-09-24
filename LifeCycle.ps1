6733b19684127c4089e7b0ae5c8fb8dd2abc68fd
Ivo Dias <igd753@gmail.com>
2020-09-24 13:58:59 -0300

    Create

---
+++ LifeCycle.ps1
@@ -0,0 +1,28 @@
+# Instala modulo
+Install-Module Az.Storage -Confirm:$false -Force -AllowClobber
+Import-Module Az.Storage -Force
+
+# Configuracoes
+$data = Get-Date -Format ddMMyyyy-hhmm
+$nome = "container-$data"
+$contaStorage = ""
+$chaveStorage = ''
+
+try {
+    # Conecta no Azure
+    $contexto = New-AzStorageContext -StorageAccountName "$contaStorage" -StorageAccountKey $chaveStorage
+    # Cria o container novo
+    Write-Host "Criando Container: $nome"
+    New-AzStorageContext -Name "$nome" -Context $contexto 
+    # Remove o mais antigo
+    $containerAntigo = Get-AzStorageContext -Name container* -Context $contexto
+    $containerAntigo = $containerAntigo[0].Name
+    Write-Host "Removendo o container: $containerAntigo"
+    Remove-AzStorageContext -Name "$containerAntigo" -Context $contexto 
+
+}
+catch {
+    # Mostra mensagem de erro
+    Write-Host "Erro ao fazer o procedimento"
+    Write-Host "Erro: $_.Exception.Message"
+}
